77276b3b79ee9a3fa283474eaedd93278b2f87cc
Sergey Voitenko <pb@tiptop.kiev.ua>
2016-03-11 16:03:03 +0200

    ver2

--- index.php
+++ index.php
@@ -9,6 +9,15 @@ print_r($array);
 echo "</pre>";
 echo "$array[name] $array[surname] $array[agerand]";
 
+$array2 = [
+    ['Артем', 'Мельник', rand(18,60)],
+    ['Никита', 'Шевченко', rand(18,60)],
+    ['Дмитрий', 'Бойко', rand(18,60)]
+];
+echo "<pre>";
+print_r($array2);
+echo "</pre>";
+
 ?>
 
 
